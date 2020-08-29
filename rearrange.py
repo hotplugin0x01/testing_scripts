c97d471503f5af363a95026a821959793336cf49
Abdul Wassay <wassaysardar35@gmail.com>
2020-08-30 02:51:45 +0500

    Fix TypeError in rearrange_name function
    Add one missing parameter in regex search function.

--- rearrange.py
+++ rearrange.py
@@ -9,7 +9,7 @@ def rearrange_name(name):
     format: 'lastName, firstName'
     returns 'firstName lastName'
     """
-    result = re.search(r'^([\w .]*), ([\w .]*)$')
+    result = re.search(r'^([\w .]*), ([\w .]*)$', name)
     if result == None:
         return name
     return '{} {}'.format(result[2], result[1])
